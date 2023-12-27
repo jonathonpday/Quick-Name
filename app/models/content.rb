28fc220a4102e3bf4d7978b8c15d2320907ad25e
jonathonpday <jonathonpday@me.com>
2023-12-27 11:22:32 +0000

    Add change to allow sign-in

--- app/models/content.rb
+++ app/models/content.rb
@@ -8,7 +8,7 @@ class Content < ApplicationRecord
     image.variant(resize_to_limit: [800, 800]).processed
   end
 
-  def image_as_mini_thumbnail
-    image.variant(resize_to_limit: [400, 400]).processed
-  end
+#   def image_as_mini_thumbnail
+#     image.variant(resize_to_limit: [400, 400]).processed
+#   end
 end
